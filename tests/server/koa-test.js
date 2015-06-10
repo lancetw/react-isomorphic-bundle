6fb919dd03e31b9e6f712b5805ab7c66fd15ce6c
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2015-06-11 02:20:45 +0800

    travis-ci problem

---
+++ tests/server/koa-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const app = require('src/server');
+const request = require('co-supertest').agent(app.listen());
+
+describe('index', function () {
+  describe('read', function () {
+    it('GET /', function*(){
+      yield request.get('/')
+        .expect(200).end();
+    });
+  });
+});
