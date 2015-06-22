2dd44c065c9752d6bd33e0bb39cdafdbf917bd78
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2015-06-22 20:12:00 +0800

    fix reload token missing problem

--- src/shared/components/addon/require-auth.js
+++ src/shared/components/addon/require-auth.js
@@ -2,8 +2,10 @@
 
 import debug from 'debug'
 import { Router } from 'react-router'
+import { load } from 'shared/actions/AuthActions'
 
 export default function (nextState, transition) {
+  this.redux.dispatch(load())
   const isAuthenticated = this.redux.getState().auth.isAuthenticated
   debug('dev')('isAuthenticated', isAuthenticated)
   if (!isAuthenticated)
