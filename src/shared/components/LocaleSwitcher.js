2cae9789e97c0be00551218aa6ce87b0bd8f5e64
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2015-06-26 15:42:58 +0800

    update locale switcher

--- src/shared/components/LocaleSwitcher.js
+++ src/shared/components/LocaleSwitcher.js
@@ -1,13 +1,16 @@
 import React, { PropTypes } from 'react'
 import counterpart from 'counterpart'
 import DocumentTitle from 'shared/components/addon/document-title'
+import * as LocaleActions from '../actions/AuthActions'
 
 export default class LocaleSwitcher extends React.Component {
 
   constructor (props) {
     super(props)
+  }
 
-    counterpart.onLocaleChange(::this.handleLocaleChange)
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired
   }
 
   static contextTypes = {
@@ -16,16 +19,14 @@ export default class LocaleSwitcher extends React.Component {
 
   handleChange (e) {
     counterpart.setLocale(e.target.value)
-  }
-
-  handleLocaleChange () {
-    //document.title = DocumentTitle.rewind()
+    const { dispatch } = this.props
+    dispatch(LocaleActions.sync(e.target.value))
   }
 
   render () {
     return (
-      <div className="item ui mini locale-switcher">
-        <select ref="test"
+      <div className="item ui locale-switcher">
+        <select id="locale"
           defaultValue={counterpart.getLocale()}
           onChange={this.handleChange}>
           <option value="zh-hant-tw">正體中文</option>
