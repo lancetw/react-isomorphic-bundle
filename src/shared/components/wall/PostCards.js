c8151216b04c183818059e6e67fa5c99a05eb6ed
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2015-07-30 11:06:25 +0800

    clean up

--- src/shared/components/wall/PostCards.js
+++ src/shared/components/wall/PostCards.js
@@ -32,20 +32,12 @@ export default class PostCards extends BaseComponent {
   }
 
   isScrollToLoad (threshold=1) {
-    console.log('---')
-    console.log(this.getDocHeight())
-    console.log('---')
-    console.log(document.body.offsetHeight)
-    console.log(document.body.clientHeight)
-    console.log(document.documentElement.offsetHeight)
-    console.log(document.documentElement.clientHeight)
-    console.log('---')
     let elemHeight = this.getDocHeight() - this.getOffsetHeight()
     return $(document).scrollTop() >= parseInt(elemHeight * threshold, 10)
   }
 
   handleScroll (event) {
-    const threshold = 0.9
+    const threshold = 0.7
     if (this.isScrollToLoad(threshold))
       this.setState({ shouldLoadFunc: true })
   }
