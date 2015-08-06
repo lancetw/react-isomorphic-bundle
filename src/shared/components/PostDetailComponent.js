98999944d9835fe799a4d6ab1d4220a655ec8ecd
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2015-08-06 08:43:07 +0800

    add missing pages

---
+++ src/shared/components/PostDetailComponent.js
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react'
+import { BaseComponent } from 'shared/components'
+import moment from 'moment'
+import counterpart from 'counterpart'
+import GMap from 'shared/components/addon/maps/gmap'
+
+const { CSSTransitionGroup } = React.addons
+
+export default class Post extends BaseComponent {
+
+  constructor (props) {
+    super(props)
+
+    counterpart.onLocaleChange(::this.handleLocaleChange)
+    this.releaseTimeout = undefined
+  }
+
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+    map: PropTypes.object.isRequired,
+    params: PropTypes.object.isRequired,
+    setPin: PropTypes.func.isRequired
+  }
+
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  }
+
+  handleLocaleChange (newLocale) {
+
+  }
+
+  componentWillUnmount () {
+    if (this.op)
+      clearTimeout(this.releaseTimeout)
+  }
+
+  render () {
+    const Translate = require('react-translate-component')
+    const { content } = this.props.post
+
+    return (
+      <main className="ui two column stackable centered page grid">
+        <div className="column">
+          <div className="ui fluid card">
+            <div className="content">
+              <i className="right floated like icon"></i>
+              <i className="right floated star icon"></i>
+              <div className="header">{content.title}</div>
+              <div className="description">
+                <p>{content.content}</p>
+              </div>
+            </div>
+            <div className="extra content">
+              <span className="left floated like">
+                <i className="like icon"></i>
+                Like
+              </span>
+              <span className="right floated star">
+                <i className="star icon"></i>
+                Favorite
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <GMap
+            ref="gmap"
+            {...this.props.map}
+          />
+        </div>
+      </main>
+    )
+  }
+}
+
---
+++ src/shared/components/PostDetailHandler.js
@@ -0,0 +1,68 @@
+import React, { PropTypes } from 'react'
+import PostDetail from './PostDetailComponent'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as PostActions from '../actions/PostActions'
+import * as MapActions from '../actions/MapActions'
+import { updateTitle } from '../actions/LocaleActions'
+import DocumentTitle from './addon/document-title'
+
+@connect(state => ({
+  post: state.post,
+  map: state.map
+}))
+export default class PostDetailHandler extends React.Component {
+
+  constructor (props, context) {
+    super(props, context)
+    const dispatch = context.store.dispatch
+    dispatch(updateTitle('title.post_detail'))
+
+    const { getState } = context.store
+    const id = props.params.id
+    Promise.all([
+      dispatch(PostActions.show(id))
+    ]).then(() => {
+      const map = {
+        place: getState().post.content.place,
+        lat: getState().post.content.lat,
+        lng: getState().post.content.lng
+      }
+      setTimeout(() => {
+        dispatch(MapActions.setPin(map))
+        console.log(getState().map)
+      }, 1000)
+
+    })
+  }
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    params: PropTypes.object.isRequired,
+    post: PropTypes.object.isRequired
+  }
+
+  static contextTypes = {
+    store: PropTypes.object.isRequired
+  }
+
+  static async routerWillRun ({ dispatch, params }) {
+    const id = params.id
+    await dispatch(PostActions.show(id))
+  }
+
+  render () {
+    const _t = require('counterpart')
+    const { dispatch } = this.props
+    return (
+      <DocumentTitle title={_t('title.post_detail')}>
+        <PostDetail
+          {...bindActionCreators(PostActions, dispatch)}
+          {...bindActionCreators(MapActions, dispatch)}
+          {...this.props}
+        />
+      </DocumentTitle>
+    )
+  }
+
+}
