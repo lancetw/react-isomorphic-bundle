0163589e919cf4814a0593042aeb5838d3ba66bd
Hsin-lin Cheng <lancetw@gmail.com>
2019-07-23 18:07:40 +0800

    Refine server side session

--- src/server/koa.js
+++ src/server/koa.js
@@ -109,7 +109,7 @@ app.use(favicon(path.join(__dirname, '../../images/app/v2.3-t/favicon.ico')))
 app.keys = require('config').app.SESSION_KEYS
 app.use(
   session(
-    { store: store({ db: leveldb }) }
+    { secure: true, sameSite: 'strict', store: store({ db: leveldb }) }
   )
 )
 
