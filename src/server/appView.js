50eec2b38aa29feeccdb3e1b1b95673f49314de0
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2019-08-27 19:50:35 +0800

    Fix code format

--- src/server/appView.js
+++ src/server/appView.js
@@ -1,113 +1,113 @@
-import React from "react";
-import { renderToString } from "react-dom/server";
-import { Router, RoutingContext, match } from "react-router";
-import { createMemoryHistory } from "history";
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reduxPromise from "shared/utils/promiseMiddleware";
-import * as reducers from "shared/reducers";
-import { Provider } from "react-redux";
-import routes from "shared/routes";
-import fs from "fs";
-import path from "path";
-import nunjucks from "nunjucks";
-import Helmet from "react-helmet";
-import * as AuthActions from "shared/actions/AuthActions";
-import * as LocaleActions from "shared/actions/LocaleActions";
-import url from "url";
-import AppContainer from "shared/components/AppContainer";
-import route from "koa-route";
-import ReduxUniversalResolver from "shared/utils/redux-universal-resolver";
-import { TranslatorInit } from "./translator-helper";
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { Router, RoutingContext, match } from 'react-router'
+import { createMemoryHistory } from 'history'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reduxPromise from 'shared/utils/promiseMiddleware'
+import * as reducers from 'shared/reducers'
+import { Provider } from 'react-redux'
+import routes from 'shared/routes'
+import fs from 'fs'
+import path from 'path'
+import nunjucks from 'nunjucks'
+import Helmet from 'react-helmet'
+import * as AuthActions from 'shared/actions/AuthActions'
+import * as LocaleActions from 'shared/actions/LocaleActions'
+import url from 'url'
+import AppContainer from 'shared/components/AppContainer'
+import route from 'koa-route'
+import ReduxUniversalResolver from 'shared/utils/redux-universal-resolver'
+import { TranslatorInit } from './translator-helper'
 
-nunjucks.configure("views", {
-  autoescape: true
-});
+nunjucks.configure('views', {
+  autoescape: true,
+})
 
 // thunkify stat
 function stat(file) {
   return function(done) {
-    fs.stat(file, done);
-  };
+    fs.stat(file, done)
+  }
 }
 
 export default function(app) {
   app.use(
-    route.get("/sitemap.xml", function*(ctx, next) {
+    route.get('/sitemap.xml', function*(ctx, next) {
       try {
-        const location = "./sitemap.xml";
-        const fstat = yield stat(location);
+        const location = './sitemap.xml'
+        const fstat = yield stat(location)
         if (fstat.isFile()) {
-          this.type = path.extname(location);
-          this.body = fs.createReadStream(location);
+          this.type = path.extname(location)
+          this.body = fs.createReadStream(location)
         }
       } catch (err) {
-        this.status = 404;
-        this.body = "404";
+        this.status = 404
+        this.body = '404'
       }
     })
-  );
+  )
 
   app.use(
-    route.get("/robots.txt", function*(ctx, next) {
+    route.get('/robots.txt', function*(ctx, next) {
       try {
-        const location = "./robots.txt";
-        const fstat = yield stat(location);
+        const location = './robots.txt'
+        const fstat = yield stat(location)
         if (fstat.isFile()) {
-          this.type = path.extname(location);
-          this.body = fs.createReadStream(location);
+          this.type = path.extname(location)
+          this.body = fs.createReadStream(location)
         }
       } catch (err) {
-        this.status = 404;
-        this.body = "404";
+        this.status = 404
+        this.body = '404'
       }
     })
-  );
+  )
 
   app.use(
-    route.get("/uploads/*", function*(ctx, next) {
-      let location = path.join(__dirname, "../../", this.path);
+    route.get('/uploads/*', function*(ctx, next) {
+      let location = path.join(__dirname, '../../', decodeURI(this.path))
       try {
-        const fstat = yield stat(location);
+        const fstat = yield stat(location)
         if (fstat.isFile()) {
-          this.type = path.extname(location);
-          this.body = fs.createReadStream(location);
+          this.type = path.extname(location)
+          this.body = fs.createReadStream(location)
         }
       } catch (err) {
-        this.status = 404;
-        this.body = "404";
+        this.status = 404
+        this.body = '404'
       }
     })
-  );
+  )
 
   app.use(function*() {
-    let isCashed = this.cashed ? yield* this.cashed() : false;
+    let isCashed = this.cashed ? yield* this.cashed() : false
     if (!isCashed) {
-      const reducer = combineReducers(reducers);
-      const enhancer = applyMiddleware(thunk, reduxPromise)(createStore);
-      const store = enhancer(reducer);
-      const resolver = new ReduxUniversalResolver();
-      store.resolver = resolver;
-      store.protocol = this.protocol;
-      store.host = this.host;
-      store.query = this.query;
+      const reducer = combineReducers(reducers)
+      const enhancer = applyMiddleware(thunk, reduxPromise)(createStore)
+      const store = enhancer(reducer)
+      const resolver = new ReduxUniversalResolver()
+      store.resolver = resolver
+      store.protocol = this.protocol
+      store.host = this.host
+      store.query = this.query
 
-      const history = createMemoryHistory();
-      const location = history.createLocation(this.url);
+      const history = createMemoryHistory()
+      const location = history.createLocation(this.url)
 
       // save session token to store
       if (this.session.token && this.session.token !== null) {
-        store.dispatch(AuthActions.sync(this.session.token));
+        store.dispatch(AuthActions.sync(this.session.token))
       }
 
-      const { translator, lang } = TranslatorInit(this.getLocaleFromHeader());
-      store.dispatch(LocaleActions.sync(lang || "zh-hant-tw"));
+      const { translator, lang } = TranslatorInit(this.getLocaleFromHeader())
+      store.dispatch(LocaleActions.sync(lang || 'zh-hant-tw'))
 
-      let appString;
-      let assets;
-      let head;
-      let siteUrl;
-      let pageUrl;
+      let appString
+      let assets
+      let head
+      let siteUrl
+      let pageUrl
       const { error, redirectLocation, renderProps } = yield new Promise(
         resolve => {
           match(
@@ -116,22 +116,22 @@ export default function(app) {
               resolve({
                 error: _error,
                 redirectLocation: _redirectLocation,
-                renderProps: _renderProps
-              });
+                renderProps: _renderProps,
+              })
             }
-          );
+          )
         }
-      );
+      )
 
       if (redirectLocation) {
-        this.redirect(redirectLocation.pathname + redirectLocation.search);
-        return;
+        this.redirect(redirectLocation.pathname + redirectLocation.search)
+        return
       } else if (error) {
-        this.redirect("/");
-        return;
+        this.redirect('/')
+        return
       } else if (renderProps === null) {
-        this.redirect("/");
-        return;
+        this.redirect('/')
+        return
       }
 
       const elements = (
@@ -142,21 +142,21 @@ export default function(app) {
             </Provider>
           )}
         </AppContainer>
-      );
+      )
 
-      renderToString(elements); // need this line to collect data
-      yield resolver.dispatch(store);
-      appString = renderToString(elements);
-      head = Helmet.rewind();
+      renderToString(elements) // need this line to collect data
+      yield resolver.dispatch(store)
+      appString = renderToString(elements)
+      head = Helmet.rewind()
 
-      const env = process.env.NODE_ENV;
-      if (env === "development") {
+      const env = process.env.NODE_ENV
+      if (env === 'development') {
         assets = fs.readFileSync(
-          path.resolve(__dirname, "../../storage/webpack-stats.json")
-        );
-        assets = JSON.parse(assets);
+          path.resolve(__dirname, '../../storage/webpack-stats.json')
+        )
+        assets = JSON.parse(assets)
       } else {
-        assets = require("storage/webpack-stats.json");
+        assets = require('storage/webpack-stats.json')
       }
 
       const serverState = renderToString(
@@ -164,26 +164,26 @@ export default function(app) {
           dangerouslySetInnerHTML={{
             __html: `window.STATE_FROM_SERVER=${JSON.stringify(
               store.getState()
-            )};`
+            )};`,
           }}
         />
-      );
+      )
 
-      siteUrl = "https://" + this.host;
-      pageUrl = this.href.replace(/^http/, "https");
-      pageUrl = location.pathname === "/sync/token" ? siteUrl : pageUrl;
+      siteUrl = 'https://' + this.host
+      pageUrl = this.href.replace(/^http/, 'https')
+      pageUrl = location.pathname === '/sync/token' ? siteUrl : pageUrl
 
-      this.body = nunjucks.render("index.html", {
+      this.body = nunjucks.render('index.html', {
         appString,
         assets,
         siteUrl,
         pageUrl,
         head,
         env: process.env,
-        stateFromServer: serverState
-      });
+        stateFromServer: serverState,
+      })
 
-      resolver.clear();
+      resolver.clear()
     }
-  });
+  })
 }
