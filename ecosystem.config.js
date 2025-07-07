99b0623c6fa51eebf5ec6a5b598e2f51c7f5512f
lancetw <lancetw@gmail.com>
2025-07-07 10:20:05 +0000

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'events',
-      script: 'yarn',
-      args: 'start',
+      script: './lib/server',
+      args: '',
       interpreter: 'node',
-      node_args: '--max-old-space-size=3072 --expose-gc',
+      node_args: '--max-old-space-size=3072',
       max_memory_restart: '2560M',
-      pmx: false
+      pmx: false,
+      env: {
+        NODE_ENV: 'production'
+      }
     }
   ]
 };
